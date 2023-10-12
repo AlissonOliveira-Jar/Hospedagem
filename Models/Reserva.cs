1c69c49e5f0e29001293aecd05c91ac3f0da1578
Alisson Oliveira <alisson.dev.py@gmail.com>
2023-10-12 19:03:15 -0300

    desafio

---
+++ Models/Reserva.cs
@@ -0,0 +1,41 @@
+﻿namespace DesafioProjetoHospedagem.Models {
+    public class Reserva {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados) {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes) {
+            if (Suite.Capacidade >= hospedes.Count) {
+                Hospedes = hospedes;
+            }
+            else {
+                throw new Exception("A capacidade é maior que o número de hóspedes sendo recebido.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite) {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes() {
+            return Hospedes != null ? Hospedes.Count : 0;
+        }
+
+        public decimal CalcularValorDiaria() {
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10) {
+                valor *= 0.9M;
+            }
+
+            return valor;
+        }
+    }
+}
